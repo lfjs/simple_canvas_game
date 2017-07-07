8aff58beb85ac018063bbecd10779f51b2ad9a97
js <js82965923@live.com>
2017-07-07 10:11:04 +0800

    hero wont reset to center after hunting

--- js/game.js
+++ js/game.js
@@ -49,8 +49,10 @@ addEventListener("keyup", function (e) {
 
 // Reset the game when the player catches a monster
 var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+	hero.x ? void(0) : hero.x = canvas.width / 2;
+	hero.y ? void(0) : hero.y = canvas.height / 2;
+	//hero.x = canvas.width / 2;
+	//hero.y = canvas.height / 2;
 
 	// Throw the monster somewhere on the screen randomly
 	monster.x = 32 + (Math.random() * (canvas.width - 64));
